c78edfc4f71f77eb5e1eb193ecf5a9fd263df811
booya-tech <booya.pannachai@gmail.com>
2024-09-04 11:05:11 +0700

    Initial Commit

---
+++ SwiftUIFundamentalProject/SwiftUIFundamentalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFundamentalProjectApp.swift
+//  SwiftUIFundamentalProject
+//
+//  Created by Panachai Sulsaksakul on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFundamentalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
