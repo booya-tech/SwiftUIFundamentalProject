91040a0c1700837bb8f9dd41ad4638c03a3eb163
booya-tech <booya.pannachai@gmail.com>
2024-09-05 13:00:51 +0700

    [PN1-2] test connection github and notion

---
+++ SwiftUIFundamentalProject/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  SwiftUIFundamentalProject
+//
+//  Created by Panachai Sulsaksakul on 9/4/24.
+//
+
+import Foundation
+
+// Use enum instead of Struct due to enum can't use initializer likes Struct does.
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ SwiftUIFundamentalProject/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  SwiftUIFundamentalProject
+//
+//  Created by Panachai Sulsaksakul on 9/4/24.
+//
+
+import Foundation
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detailBackground")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
